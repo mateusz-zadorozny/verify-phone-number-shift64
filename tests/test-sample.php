57454e04f8c1a58c2793eb5387a86789f61a1712
Mateusz Zadorożny <mateusz@mpress.cc>
2026-01-28 13:46:36 +0100

    feat: US-001 - Project structure and autoloading

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Verify_Phone_Number_Shift64
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
